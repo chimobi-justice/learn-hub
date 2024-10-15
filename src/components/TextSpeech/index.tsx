0a1d5e1e7e40c4408939c3f30d1b4c7898784c7a
justice chimobi <chimobi.justice@gmail.com>
2024-10-15 19:00:51 +0100

    remove commented codes

--- src/components/TextSpeech/index.tsx
+++ src/components/TextSpeech/index.tsx
@@ -19,11 +19,19 @@ const TextSpeech: FunctionComponent<TextSpeechProps> = ({ title, content }) => {
     newUtterance.lang = "en-US";
     setUtterance(newUtterance);
 
+    newUtterance.onend = () => {
+      setIsSpeaking(false);
+      setIsPaused(false);
+    };
+
+    newUtterance.onerror = (event) => {
+      console.error("Speech synthesis error:", event.error);
+    };
+
     return () => {
       synth.cancel();
     }
   }, [title, content]);
-
 
   const handlePlay = () => {
     if (utterance && !isSpeaking) {
@@ -78,101 +86,3 @@ const TextSpeech: FunctionComponent<TextSpeechProps> = ({ title, content }) => {
 }
 
 export default TextSpeech;
-
-
-// interface TextSpeechProps {
-//   title?: string;
-//   content?: string;
-// }
-
-// const TextSpeech: FunctionComponent<TextSpeechProps> = ({ title, content }) => {
-//   const [isSpeaking, setIsSpeaking] = useState(false);
-//   const [isPaused, setIsPaused] = useState(false);
-//   const [utterance, setUtterance] = useState<SpeechSynthesisUtterance | null>(null);
-
-//   const synth = window.speechSynthesis;
-
-//   useEffect(() => {
-//     const handleVoicesChanged = () => {
-//       const voices = synth.getVoices();
-//       if (voices.length > 0) {
-//         const newUtterance = new SpeechSynthesisUtterance(`${title} ${content}`);
-//         newUtterance.voice = voices.find(voice => voice.lang === 'en-US'); // Set preferred voice
-//         newUtterance.lang = "en-US";
-
-//         newUtterance.onend = () => {
-//           setIsSpeaking(false); // Reset speaking status when finished
-//           setIsPaused(false);
-//         };
-
-//         newUtterance.onerror = (event) => {
-//           console.error("Speech synthesis error:", event.error);
-//         };
-
-//         setUtterance(newUtterance);
-//       }
-//     };
-
-//     if (synth.getVoices().length > 0) {
-//       handleVoicesChanged();
-//     } else {
-//       synth.onvoiceschanged = handleVoicesChanged;
-//     }
-
-//     return () => {
-//       synth.cancel(); // Cleanup on component unmount
-//     };
-//   }, [title, content, synth]);
-
-//   const handlePlay = () => {
-//     if (utterance && !isSpeaking) {
-//       synth.speak(utterance);
-//       setIsSpeaking(true);
-//       setIsPaused(false);
-//     } else if (isPaused) {
-//       synth.resume();
-//       setIsPaused(false);
-//     }
-//   }
-
-//   const handlePause = () => {
-//     if (synth.speaking && !isPaused) {
-//       synth.pause();
-//       setIsPaused(true);
-//     }
-//   }
-
-//   const handleStop = () => {
-//     synth.cancel();
-//     setIsSpeaking(false);
-//     setIsPaused(false);
-//   }
-
-//   return (
-//     <Flex gap={3} alignItems={"center"}>
-//       {!isSpeaking || isPaused ? (
-//         <Tooltip label={isPaused ? 'Resume' : 'Play'} placement="top">
-//           <Text as="span">
-//             <IoPlayCircleOutline size="21px" onClick={handlePlay} cursor="pointer" />
-//           </Text>
-//         </Tooltip>
-//       ) : (
-//         <Tooltip label={"Stop Listening"} placement="top">
-//           <Text as="span">
-//             <IoPlayCircle size="25px" onClick={handleStop} cursor="pointer" />
-//           </Text>
-//         </Tooltip>
-//       )}
-
-//       {isSpeaking && !isPaused && (
-//         <Tooltip label={"Pause Listening"} placement="top">
-//           <Text as="span">
-//             <IoMicCircleSharp size={"25px"} onClick={handlePause} cursor="pointer" />
-//           </Text>
-//         </Tooltip>
-//       )}
-//     </Flex>
-//   );
-// }
-
-// export default TextSpeech;
--- src/pages/Articles/components/RelatedArticles.tsx
+++ src/pages/Articles/components/RelatedArticles.tsx
@@ -7,33 +7,37 @@ import { useGetRelatedArticles } from '@hooks/article/useGetRelatedAuthorArticle
 
 const RelatedArticles: FunctionComponent = () => {
   const { id } = useParams();
-  const { data: relatedArticles } = useGetRelatedArticles(id!)
+  const { data: relatedArticles, isSuccess } = useGetRelatedArticles(id!)
 
   return (
-    <Box width="100%" maxWidth={{ base: '90%', lg: '70%' }} m="2em auto" px="10px">
-      <Heading size={"sm"} p={"10px"}>More articles from this author</Heading>
-      
-      <Box>
-      <Grid
-          templateColumns={{ base: "1fr", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
-          gap={{ base: "10px", md: "15px", lg: "20px" }}
-          px={{ base: "6px", md: "10px", lg: "15px" }}
-          py="40px"
-        >
-          {relatedArticles?.map((article: any, index: any) => (
-            <GridItem key={index} w="100%" h="100%">
-              <LatestArticleCard
-                articleImage={article?.thumbnail}
-                date={article?.created_at?.human}
-                title={article?.title}
-                description={article?.content}
-                CTA={`/articles/${article?.slug}/${article?.id}`}
-                CTAText="Read article"
-              />
-            </GridItem>
-          ))}
-        </Grid>
-      </Box>
+    <Box width="100%" maxWidth={{ base: '90%', lg: '73%' }} m="2em auto" px="10px">
+      {relatedArticles && isSuccess && (
+        <>
+          <Heading size={"sm"} p={"10px"}>More articles from this author</Heading>
+          
+          <Box>
+          <Grid
+              templateColumns={{ base: "1fr", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
+              gap={{ base: "10px", md: "15px", lg: "20px" }}
+              px={{ base: "6px", md: "10px", lg: "15px" }}
+              py="40px"
+            >
+              {relatedArticles?.map((article: any, index: any) => (
+                <GridItem key={index} w="100%" h="100%">
+                  <LatestArticleCard
+                    articleImage={article?.thumbnail}
+                    date={article?.created_at?.human}
+                    title={article?.title}
+                    description={article?.content}
+                    CTA={`/articles/${article?.slug}/${article?.id}`}
+                    CTAText="Read article"
+                  />
+                </GridItem>
+              ))}
+            </Grid>
+          </Box>
+        </>
+      )}
     </Box>
   )
 }
