be4eca16205756dc03fbff58fa6c9ab82ccda752
justice chimobi <chimobi.justice@gmail.com>
2025-01-26 16:14:43 +0100

    Add conditional execution for useGetUser based on authentication

--- src/hooks/user/useGetUser.ts
+++ src/hooks/user/useGetUser.ts
@@ -2,9 +2,10 @@ import { useQuery } from '@tanstack/react-query'
 
 import { getUser } from '@services/user'
 
-export const useGetUser = () => {
+export const useGetUser = (isAuthenticated: boolean) => {
   return useQuery({
     queryKey: ['user'],
     queryFn: getUser,
+    enabled: isAuthenticated
   })
 }
